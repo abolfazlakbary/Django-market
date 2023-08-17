e0af95f7b9995bd83f1e3ef26ddbe8a3a3fbeaca
abolfazlakbary <abolfazl.akbary132@gmail.com>
2023-08-17 16:59:00 +0330

    Models Created

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app1.models import ArticleColor, ArticleGuarantee, Article
 
-# Register your models here.
+admin.site.register(ArticleColor)
+admin.site.register(ArticleGuarantee)
+admin.site.register(Article)
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from phone_field import PhoneField
+from djmoney.models.fields import MoneyField
+from django.conf import settings
+import datetime
 
-# Create your models here.
+class ArticleColor(models.Model):
+    color = models.CharField(max_length=50)
+    def __str__(self):
+        return self.color
+    
+
+class ArticleGuarantee(models.Model):
+    guarantee_time = models.CharField(max_length=50)
+    def __str__(self):
+        return self.guarantee_time
+
+def upload_path(instance,title):
+    today = str(datetime.datetime.now())
+    name = instance.user
+    return f'images/{name}/{today}.jpg'
+    
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_path, blank=True)
+    title = models.CharField(max_length=100)
+    color = models.ForeignKey(ArticleColor, on_delete=models.CASCADE)
+    description = models.TextField()
+    publish_date = models.DateTimeField(auto_now=True)
+    cost = MoneyField(max_digits=12,decimal_places=2, default_currency='USD')
+    guarantee = models.ForeignKey(ArticleGuarantee, on_delete=models.CASCADE)
+    available = models.BooleanField(default=False)
+    phone_call = PhoneField()
+    def __str__(self):
+        return self.title
